b536ee7b948f39a6e055e2796e88f1c54a6ece5e
Ben Noordhuis <info@bnoordhuis.nl>
2023-10-18 19:13:15 +0200

    support perm5 opcode

--- quickjs.c
+++ quickjs.c
@@ -32802,6 +32802,18 @@ static void js_jit(JSContext *ctx, JSFunctionBytecode *b)
                 "}");
             pc++;
             break;
+        case 0x1A: // perm5:none 1 +5,-5
+            dbuf_putstr(&dbuf,
+                "{"
+                "JSValue tmp;"
+                "tmp = sp[-2];"
+                "sp[-2] = sp[-3];"
+                "sp[-3] = sp[-4];"
+                "sp[-4] = sp[-5];"
+                "sp[-5] = tmp;"
+                "}");
+            pc++;
+            break;
         case 0x22: // call:npop 3 +1,-1
         case 0x23: // tail_call:npop 3 +0,-1
             call_argc = get_u16(pc+1);
